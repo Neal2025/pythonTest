4452eba62fb8b8a6fe144bd9faf33608b5d04bb2
neal2025 <nealgithub@163.com>
2025-04-29 12:27:12 +0800

    add employee_onboarding.py

--- Package0320/employee_onboarding.py
+++ Package0320/employee_onboarding.py
@@ -4,7 +4,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 # from selenium.common.exceptions import NoSuchElementException, TimeoutException
-# import time
+
 
     # def common_fn(self,select, index):
     #     config = configparser.ConfigParser()
